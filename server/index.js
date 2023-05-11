ad851a4439e0dff2676cabdcd2dc963af46df280
Joaquin Arregui <116235560+joacoarregui@users.noreply.github.com>
2023-05-11 01:14:02 -0300

    a
    a

--- server/index.js
+++ server/index.js
@@ -1,25 +1,46 @@
 require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const app = express();
+const { google } = require('googleapis');
 
-
-app.use(express.static('public'));
-
-app.get('/api/tabla_usd_largo_plazo_local', async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://sheets.googleapis.com/v4/spreadsheets/1JJf7z5h9-RiJr0rWUpGpdTQPBfIslbyNXtIurq1660s/values/FCI TABLAS USD!D83:I102?key=${process.env.API_KEY}`
-    );
-    res.json(response.data.values);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error al obtener los datos de la tabla');
-  }
+// ...
+console.log(process.env);
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+  },
+  scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
 });
 
-const PORT = process.env.PORT || 3000;
+const sheets = google.sheets({ version: 'v4', auth });
 
-app.listen(PORT, () => {
-  console.log(`Servidor iniciado en el puerto ${PORT}`);
+const spreadsheetId = '1JJf7z5h9-RiJr0rWUpGpdTQPBfIslbyNXtIurq1660s';
+const range = 'FCI TABLAS USD!D83:I102';
+
+app.get('/valores/tabla_usd_largo_plazo_local', async (req, res) => {
+  console.log('Endpoint llamado');
+  try {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}`;
+    const response = await axios.get(url);
+    console.log(response.data); // Verificar si la respuesta contiene los datos esperados
+
+    const responseSheets = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range
+    });
+    console.log(responseSheets.data); // Verificar si la respuesta de Google Sheets contiene los datos esperados
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    console.log(error.response.data);
+    res.status(500).send('Error al obtener los datos de la tabla');
+  }
 });
