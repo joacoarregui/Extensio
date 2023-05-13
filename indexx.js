3939ccfa4678531dcc4df7662cae100168885b4d
Joaquin Arregui <116235560+joacoarregui@users.noreply.github.com>
2023-05-13 00:35:15 -0300

    4
    4

--- indexx.js
+++ indexx.js
@@ -1,16 +1,7 @@
-$(document).ready(function() {
-    $('#tablaDatos').DataTable({
-      "processing": true,
-      "serverSide": false,
-      "ajax": {
-        "url": "https://sheets.googleapis.com/v4/spreadsheets/1jyzHFthvhOcBf9oDwA7XrkJ1ZS1wGtFZu7rHE0QbJD8/values/high_value!B37:J57?key=AIzaSyDD6wEmrY190Br80MQCrVDCYgZx4KJic_Y",
-        "dataSrc": "values"
-      },
-      "columns": [
-        { "data": 6 },
-        { "data": 0 },
-        { "data": 4 },
-        { "data": 8 },
-      ]
-    });
-  });
+require('dotenv').config();
+
+const apiKey = process.env.API_KEY;
+console.log(apiKey);
+function getApiKey() {
+  return apiKey;
+}
--- server/index.js
+++ server/index.js
@@ -1,46 +1,28 @@
 require('dotenv').config();
-const { google } = require('googleapis');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const api_key = process.env.API_KEY;
+const PORT = process.env.PORT || 3000;
 
-// ...
-console.log(process.env);
-const auth = new google.auth.GoogleAuth({
-  credentials: {
-    type: process.env.TYPE,
-    project_id: process.env.PROJECT_ID,
-    private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.CLIENT_EMAIL,
-    client_id: process.env.CLIENT_ID,
-    auth_uri: process.env.AUTH_URI,
-    token_uri: process.env.TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
-  },
-  scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
-});
 
-const sheets = google.sheets({ version: 'v4', auth });
+// Configuración del servidor
+app.use(express.static('public'));
 
-const spreadsheetId = '1JJf7z5h9-RiJr0rWUpGpdTQPBfIslbyNXtIurq1660s'; 
-const range = 'FCI TABLAS USD!D83:I102';
-const google_sheet_apikey = process.env.PRIVATE_KEY_ID;
-app.get('/valores/tabla_usd_largo_plazo_local', async (req, res) => {
-  console.log('Endpoint llamado');
+// Rutas
+
+app.get('/api/tabla_usd_largo_plazo_local', async (req, res) => {
   try {
-    const url = 'https://sheets.googleapis.com/v4/spreadsheets/'+ spreadsheetId +'/values/'+ range + '?key=' +google_sheet_apikey;
-    const response = await axios.get(url);
-    console.log(response.data); // Verificar si la respuesta contiene los datos esperados
-    console.log(url);
-    const responseSheets = await sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range
-    });
-    console.log(responseSheets.data); // Verificar si la respuesta de Google Sheets contiene los datos esperados
-
-    res.json(response.data);
+    const response = await axios.get(
+      `https://sheets.googleapis.com/v4/spreadsheets/1JJf7z5h9-RiJr0rWUpGpdTQPBfIslbyNXtIurq1660s/valuesFCI%20TABLAS%20USD!D83:I102?key=${process.env.API_KEY}`
+    );
+    res.json(response.data.values);
   } catch (error) {
     console.error(error);
-    console.log(error.response.data);
     res.status(500).send('Error al obtener los datos de la tabla');
   }
 });
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
+  });
