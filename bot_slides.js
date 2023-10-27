b1fdc8cc7ef26c323ab55e078db18e55d5279471
Joaquin Arregui <116235560+joacoarregui@users.noreply.github.com>
2023-10-27 14:15:49 -0300

    s
    s

--- bot_slides.js
+++ bot_slides.js
@@ -17,40 +17,38 @@ async function main() {
 
 async function authorizeWithServiceAccount() {
   const auth = new google.auth.GoogleAuth({
-    keyFile: 'credentials_slides.json', 
+    keyFile: 'credentials_slides.json',
     scopes: ['https://www.googleapis.com/auth/presentations'],
   });
   return auth.getClient();
 }
 
 async function downloadSlidesAsPNG(presentationId, auth) {
-    // Verifica que la presentación exista
-    const response = await slides.presentations.get({
-      id: presentationId,
+  // Verifica que la presentación exista
+  const response = await slides.presentations.get({
+    id: presentationId,
+    auth,
+  });
+
+  if (!response.data) {
+    throw new Error('La presentación no existe');
+  }
+
+  // Descarga las diapositivas
+  const slidesToExport = [1, 2, 3, 4]; // Cambia esto a las diapositivas que deseas exportar.
+
+  for (const slideIndex of slidesToExport) {
+    const response = await slides.presentations.pages.get({
+      presentationId,
+      pageObjectId: `g${slideIndex}`,
       auth,
     });
-  
-    if (!response.data) {
-      throw new Error('La presentación no existe');
-    }
-  
-    // Descarga las diapositivas
-    const slidesToExport = [1, 2, 3, 4]; // Cambia esto a las diapositivas que deseas exportar.
-  
-    for (const slideIndex of slidesToExport) {
-      const response = await slides.presentations.pages.get({
-        presentationId,
-        pageObjectId: `g${slideIndex}`,
-        auth,
-      });
-  
-      const imageUrl = response.data.pageElements[0].image.imageUri;
-      // Descarga la imagen y guárdala como PNG
-      // ...
-    }
+
+    const imageUrl = response.data.pageElements[0].image.imageUri;
+    // Descarga la imagen y guárdala como PNG
+    // ...
   }
-  
-  // Llama al método downloadSlidesAsPNG() con la ID de la presentación
-  await downloadSlidesAsPNG('1EfM-DXU7OmDxK8kefE8EmjmKHI_xDcqQ4zJJv1so8Zk', auth);
-  
+}
+
+// Llama al método downloadSlidesAsPNG() desde dentro de la función main()
 main();
